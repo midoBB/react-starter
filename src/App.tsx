c3f4f19a809bf84b9fc42b1ad92955a5f55edf7b
Mohamed Hamdi <haamdi@outlook.com>
2022-08-14 21:15:31 +0100

    Finished inital setup

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Henlo</div>;
+}
+console.log(import.meta.env.VITE_API_URL);
+export default App;
---
+++ src/env.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_IMPORT: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
